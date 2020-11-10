aa6fe7a069fbd491caefbb4973963512031672d6
Nyannsuke01 <shingo_373_100_10ve@yahoo.co.jp>
2020-11-11 03:31:11 +0900

    ViewControllerの責務をVIewModelに移植しました。

--- MVVMFromMVC/Controllers/WeatherViewController.swift
+++ MVVMFromMVC/Controllers/WeatherViewController.swift
@@ -31,18 +31,7 @@ import UIKit
 class WeatherViewController: UIViewController {
 
   private  let viewModel = WeatherViewModel()
-  private let geocoder = LocationGeocoder()
-  private let defaultAddress = "McGaheysville, VA"
-  private let dateFormatter: DateFormatter = {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "EEEE, MMM d"
-    return dateFormatter
-  }()
-  private let tempFormatter: NumberFormatter = {
-    let tempFormatter = NumberFormatter()
-    tempFormatter.numberStyle = .none
-    return tempFormatter
-  }()
+
   
   @IBOutlet weak var cityLabel: UILabel!
   @IBOutlet weak var dateLabel: UILabel!
@@ -51,38 +40,43 @@ class WeatherViewController: UIViewController {
   @IBOutlet weak var forecastSummary: UITextView!
   
   override func viewDidLoad() {
-    geocoder.geocode(addressString: defaultAddress) { [weak self] locations in
-      guard
-        let self = self,
-        let location = locations.first
-        else {
-          return
-        }
-      self.cityLabel.text = location.name
-      self.fetchWeatherForLocation(location)
-    }
-  }
 
-  func fetchWeatherForLocation(_ location: Location) {
-    //1気象サービスを呼び出して、その場所の緯度と経度を渡します。
-    WeatherbitService.weatherDataForLocation(
-      latitude: location.latitude,
-      longitude: location.longitude) { [weak self] (weatherData, error) in
-      //2気象サービスのコールバックによって提供された気象データでビューを更新します
-      guard
-        let self = self,
-        let weatherData = weatherData
-        else {
-          return
-        }
-      self.dateLabel.text =
-        self.dateFormatter.string(from: weatherData.date)
-      self.currentIcon.image = UIImage(named: weatherData.iconName)
-      let temp = self.tempFormatter.string(
-        from: weatherData.currentTemp as NSNumber) ?? ""
-      self.currentSummaryLabel.text =
-        "\(weatherData.description) - \(temp)℉"
-      self.forecastSummary.text = "\nSummary: \(weatherData.description)"
+    viewModel.locationName.bind { [weak self] locationName in
+      self?.cityLabel.text = locationName
+    }
+
+    viewModel.date.bind { [weak self] date in
+      self?.dateLabel.text = date
+    }
+
+    viewModel.icon.bind { [weak self] image in
+      self?.currentIcon.image = image
+    }
+
+    viewModel.summary.bind { [weak self] summary in
+      self?.currentSummaryLabel.text = summary
+    }
+
+    viewModel.forecastSummary.bind { [weak self] forecast in
+      self?.forecastSummary.text = forecast
     }
   }
+    @IBAction func promptForLocation(_ sender: Any) {
+        //1
+        let alert = UIAlertController(
+            title: "Choose location",
+            message: nil,
+            preferredStyle: .alert)
+        alert.addTextField()
+        //2
+        let submitAction = UIAlertAction(
+            title: "Submit",
+            style: .default) { [unowned alert, weak self] _ in
+            guard let newLocation = alert.textFields?.first?.text else { return }
+            self?.viewModel.changeLocation(to: newLocation)
+        }
+        alert.addAction(submitAction)
+        //3
+        present(alert, animated: true)
+    }
 }
